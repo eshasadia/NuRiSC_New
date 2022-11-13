88f94c884c24151663ab46164438b9e8ce57adaa
Esha Sadia <45667822+eshasadia@users.noreply.github.com>
2022-11-13 22:12:12 +0500

    Update MSSA_Net.py

--- nurisc/models/MSSA_Net.py
+++ nurisc/models/MSSA_Net.py
@@ -166,7 +166,7 @@ def transformer(x):
 def encoder1(inputs):
     skip_connections = []
 
-    model = VGG19(include_top=False, weights='imagenet', input_tensor=inputs)
+    model = VGG19(include_top=True, weights='imagenet', input_tensor=inputs)
     names = ["block1_conv2", "block2_conv2", "block3_conv4", "block4_conv4","block5_conv5"]
     for name in names:
         skip_connections.append(model.get_layer(name).output)
